e66288504cce3ec566da01a7e1921c46de389b88
Jay Pluckrose <jay.pluckrose.work@gmail.com>
2023-08-05 12:32:00 +0300

    Updating test case to be more specific and updated plants.yml fixtures for general testing later

--- watering_app/test/controllers/plant_controller_test.rb
+++ watering_app/test/controllers/plant_controller_test.rb
@@ -1,9 +1,9 @@
 require "test_helper"
 
 class PlantControllerTest < ActionDispatch::IntegrationTest
-  test "successfully saves correct json message" do
+  test "successfully posts json message and returns OK response" do
     post "/plants",
       params: {plant: {name: "spider plant", moisture_level: 22, water_pump_status: "off", battery_level: 33 }}
-    assert_response :success
+    assert_response 200
   end
 end
