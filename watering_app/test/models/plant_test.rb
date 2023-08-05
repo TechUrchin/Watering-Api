8de2b98ea2ed08e01ddb950b313714091e01aa89
Jay Pluckrose <jay.pluckrose.work@gmail.com>
2023-08-05 14:50:18 +0300

    Creating tests for the models that fail successfully

--- watering_app/test/models/plant_test.rb
+++ watering_app/test/models/plant_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class PlantTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validate correct moisture level" do
+    assert_not_nil(Plant.new({name: "spider plant", moisture_level: 22, water_pump_status: "off", battery_level: 33 }))  
+  end
+
+  test "fail on negative moisture levels" do
+    assert_raises(StandardError) do
+      Plant.new({name: "spider plant", moisture_level: -1, water_pump_status: "off", battery_level: 33 })
+    end  
+  end
+
+  test "fail on moisture level above 100" do
+    assert_raises(StandardError) do
+      Plant.new({name: "spider plant", moisture_level: 101, water_pump_status: "off", battery_level: 33 })
+    end 
+  end
 end
